bb02a7a1685ab99dc046d841ae758b36a417ee2e
Ricky Liu <rorix12321@gmail.com>
2018-08-23 16:35:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ contacts.each do |person, data|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         if flavor == "strawberry"
-          contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+          :favorite_icecream_flavors.delete("strawberry")
         end
       end
     end
