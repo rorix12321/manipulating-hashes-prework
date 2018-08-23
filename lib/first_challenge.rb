95ef15ff4fe75be160d81d073892d31279d46f08
Ricky Liu <rorix12321@gmail.com>
2018-08-23 16:24:02 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -27,4 +27,3 @@ first_challenge.each do |person, data|
   #remember to return your newly altered contacts hash!
   contacts
 end
-
